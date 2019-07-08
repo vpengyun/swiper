f764b457eeb29681b66a433d591506167022348d
vpengyun <vpengyun@163.com>
2019-07-07 21:58:13 -0400

    user

--- user/api.py
+++ user/api.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def verify_phone(request):
+    """
+    验证手机号
+    :param request:
+    :return:
+    """
+    phone_num = request.POST.get('phone_num','')
+    phone_num = phone_num.strip()
+
+
+
+    pass
+def login(request):
+    """
+    用户登录
+    :param request:
+    :return:
+    """
+    pass
+def get_profile(request):
+    """
+    获取用户信息
+    :param request:
+    :return:
+    """
+    pass
+
+def set_profile(request):
+    """
+    设置用户信息
+    :param request:
+    :return:
+    """
+    pass
+
+def upload_avatar(request):
+    """
+    更新用户信息
+    :param request:
+    :return:
+    """
+    pass
+
+
